79314cf3a3294aac809acd0b6cbc4b8d547716e9
larrytao05 <larrytao100@gmail.com>
2019-09-15 10:25:37 -0400

    first commit of september 15th 2019

---
+++ class-0915/rectangle.py
@@ -0,0 +1,3 @@
+base = 10
+height = 4
+print("Area of a rectangle with a base of 10 and height of 4: ", base * height)
---
+++ class-0915/triangle_right.py
@@ -0,0 +1,3 @@
+base = 10
+height = 4
+print("Area of a right triangle with a base of 10 and height of 4: ", base * height / 2)
